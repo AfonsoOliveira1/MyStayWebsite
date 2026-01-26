ce011de2ff89988c68e68969a14ad31e3d2e1713
Afonso Oliveira <afonso24.oliveira@gmail.com>
2026-01-26 17:08:53 +0000

    Views login, register e Home

---
+++ Services/Authentication.cs
@@ -0,0 +1,45 @@
+﻿/*
+using Microsoft.AspNetCore.Identity;
+
+namespace Booking.web.Services
+{
+    public class Authentication
+    {
+        private IHttpClientFactory _context;
+        private PasswordHasher<> _passwordHasher;
+        private User _currentuser;
+
+        public Authentication()
+        {
+            _passwordHasher = new PasswordHasher<User>();
+        }
+        public User? FinByLogin(string login)
+        {
+            if (string.IsNullOrWhiteSpace(login))
+                return null;
+
+            return _context.User.FirstOrDefault(u => u.UserName.Equals(login) || u.Email.Equals(login));
+        }
+        public User CurrentUser() => _currentuser;
+
+        public bool Login(string username, string password)
+        {
+            User user = FinByLogin(username);
+            if (user == null) return false;
+
+            PasswordVerificationResult result = _passwordHasher.VerifyHashedPassword(user, user.Passwordhash, password);
+            if (result == PasswordVerificationResult.Success)
+            {
+                _currentuser = user;
+                return true;
+            }
+            return false;
+        }
+
+        public void Logout()
+        {
+            _currentuser = null;
+        }
+    }
+}
+*/
