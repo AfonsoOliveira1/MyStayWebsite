f9bce4c3118d9332f0e99e2e1b872cf4cd3da2f0
Tomas Theotonio <I230051@inete.net>
2026-02-02 16:50:09 +0000

    ja com as alteracoes

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Booking.web.Models;
+﻿using Booking.web.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using System.Net.Http.Json;
+using System.Net.Mail;
+using System.Security.Claims;
 
 namespace Booking.web.Controllers
 {
@@ -85,7 +87,89 @@ namespace Booking.web.Controllers
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home");
         }
-    }
+        [HttpGet]
+        public IActionResult VerifyCode(string Email)
+        {
+            string code = new Random().Next(100000, 999999).ToString();
 
-    
+            TempData["VerificationCode"] = code;
+            TempData["Email"] = Email;
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("mystaystaff@gmail.com");
+            mail.To.Add(Email);
+            mail.Subject = "MyStay Verification Code (2FA)";
+            mail.Body = $"Automated Email from MyStay,\nYour verification code is: {code}";
+
+            //ENVIO REAL
+            SmtpClient smtp;
+            string domain = Email.Split('@')[1].ToLower();
+            try
+            {
+                if (domain.Contains("gmail.com"))
+                {
+                    smtp = new SmtpClient("smtp.gmail.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else if (domain.Contains("outlook.com") || domain.Contains("hotmail.com") || domain.Contains("live.com"))
+                {
+                    smtp = new SmtpClient("smtp.office365.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else if (domain.Contains("yahoo.com"))
+                {
+                    smtp = new SmtpClient("smtp.mail.yahoo.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error in sending the email.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Failed to send email: " + ex.Message);
+                return View(new VerifyCodeModel());
+            }
+
+            return View(new VerifyCodeModel());
+        }
+
+        [HttpPost]
+        public IActionResult VerifyCode(VerifyCodeModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            string correctCode = TempData["VerificationCode"]?.ToString();
+
+            if (correctCode == null)
+            {
+                ModelState.AddModelError("", "Verification expired. Request a new code.");
+                return View(model);
+            }
+
+            if (model.Code != correctCode)
+            {
+                ModelState.AddModelError("", "Invalid Code");
+                return View(model);
+            }
+
+            return RedirectToAction("Profile");
+        }
+
+
+    }
 }
---
+++ Models/VerifyCodeModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.web.Models
+{
+    public class VerifyCodeModel
+    {
+        [Required]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "O código deve ter 6 dígitos.")]
+        public string Code { get; set; }
+    }
+
+}
