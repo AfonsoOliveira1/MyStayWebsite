1ac81477c272864c34087055cc625fdbcca28848
AfonsoOliveira1 <afonso24.oliveira@gmail.com>
2026-02-05 15:57:01 +0000

    asd

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Json;
 using System.Net.Mail;
+using System.Reflection;
 using System.Security.Claims;
 
 namespace Booking.web.Controllers
@@ -35,28 +36,20 @@ namespace Booking.web.Controllers
                 if (loginResponse != null && loginResponse.User != null)
                 {
                     var claims = new List<Claim>
-                    {
-                        new Claim(ClaimTypes.NameIdentifier, loginResponse.User.Id.ToString()),
-                        new Claim(ClaimTypes.Name, loginResponse.User.Name ?? loginResponse.User.Email),
-                        new Claim(ClaimTypes.Email, loginResponse.User.Email),
-                        new Claim(ClaimTypes.Role, loginResponse.User.Role),
-                        
-                        new Claim("JWToken", loginResponse.Token)
-                    };
+            {
+                // Agora o compilador já reconhece o .Id porque usamos UserViewModel
+                new Claim(ClaimTypes.NameIdentifier, loginResponse.User.Id.ToString()),
+                new Claim(ClaimTypes.Name, loginResponse.User.Name ?? loginResponse.User.Email),
+                new Claim(ClaimTypes.Email, loginResponse.User.Email),
+                new Claim(ClaimTypes.Role, loginResponse.User.Role),
+                new Claim("JWToken", loginResponse.Token)
+            };
 
                     var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                    //guardar cookie e token
-                    var authProperties = new AuthenticationProperties();
-                    authProperties.StoreTokens(new[]
-                    {
-                        new AuthenticationToken { Name = "access_token", Value = loginResponse.Token }
-                    });
-                   
                     await HttpContext.SignInAsync(
                         CookieAuthenticationDefaults.AuthenticationScheme,
-                        new ClaimsPrincipal(claimsIdentity),
-                        authProperties //  as propriedades com o token 
+                        new ClaimsPrincipal(claimsIdentity)
                     );
 
                     TempData["Message"] = "Welcome back!";
@@ -76,6 +69,8 @@ namespace Booking.web.Controllers
             if (!ModelState.IsValid) return View(model);
 
             var client = _clientFactory.CreateClient("Booking.API");
+
+
             var response = await client.PostAsJsonAsync("api/users/register", model);
 
             if (response.IsSuccessStatusCode)
@@ -93,7 +88,125 @@ namespace Booking.web.Controllers
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home");
         }
+        public void Email2FA(string toEmail)
+        {
+            string code = new Random().Next(100000, 999999).ToString();
 
-        
+            TempData["VerificationCode"] = code;
+            TempData["Email"] = toEmail;
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("mystaystaff@gmail.com");
+            mail.To.Add(toEmail);
+            mail.Subject = "MyStay Verification Code (2FA)";
+            mail.Body = $"Automated Email from MyStay,\nYour verification code is: {code}";
+
+            //ENVIO REAL
+            SmtpClient smtp;
+            string domain = toEmail.Split('@')[1].ToLower();
+            try
+            {
+                if (domain.Contains("gmail.com"))
+                {
+                    smtp = new SmtpClient("smtp.gmail.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else if (domain.Contains("outlook.com") || domain.Contains("hotmail.com") || domain.Contains("live.com"))
+                {
+                    smtp = new SmtpClient("smtp.office365.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else if (domain.Contains("yahoo.com"))
+                {
+                    smtp = new SmtpClient("smtp.mail.yahoo.com", 587)
+                    {
+                        Credentials = new NetworkCredential("mystaystaff@gmail.com", "swqp mdpr bysr jxtz"),
+                        EnableSsl = true
+                    };
+                    smtp.Send(mail);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error in sending the email.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Failed to send email: " + ex.Message);
+            }
+        }
+        public bool Verify2FA(VerifyCodeModel model)
+        {
+            string correctCode = TempData["VerificationCode"]?.ToString();
+
+            if (correctCode == null)
+            {
+                ModelState.AddModelError("", "Verification expired. Request a new code.");
+                return false;
+            }
+
+            if (model.Code != correctCode)
+            {
+                ModelState.AddModelError("", "Invalid Code");
+                return false;
+            }
+            return true;
+        }
+        [HttpGet]
+        public IActionResult VerifyCode(string Email)
+        {
+            Email2FA(Email);
+            return View(new VerifyCodeModel());
+        }
+
+        [HttpPost]
+        public IActionResult VerifyCode(VerifyCodeModel model)
+        {
+            if (Verify2FA(model) == false)
+                return View(model);
+            else
+                return RedirectToAction("Profile");
+        }
+
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            var user = new ProfileViewModel
+            {
+                Name = User.Identity.Name ?? "Unknown",
+                Email = User.FindFirst(ClaimTypes.Email)?.Value ?? "Unknown",
+                Role = User.FindFirst(ClaimTypes.Role)?.Value ?? "Unknown",
+            };
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProfileEdit(ProfileViewModel profile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Profile");
+            }
+
+            if (profile.NewPassword != profile.ConfirmPassword)
+            {
+                ModelState.AddModelError("", "New password and confirmation do not match.");
+                return View("Profile", profile);
+            }
+
+            var client = _clientFactory.CreateClient("Booking.API");
+            var response = await client.PutAsJsonAsync("api/users/update", profile);
+
+            TempData["Sucess"] = "Profile updated successfully!";
+            return View("Profile", profile);
+        }
     }
 }
