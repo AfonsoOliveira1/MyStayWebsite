5149d95617f5a8f418a50e4f0eed055e04e1f216
Afonso Oliveira <afonso24.oliveira@gmail.com>
2026-02-06 01:34:05 +0000

    muito erro fixed e falta meter 2fa no register a justar margin no error model

---
+++ Models/UserUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Booking.web.Models
+{
+    public class UserUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ?Password { get; set; }
+        public string Role { get; set; } // "Admin", "Customer", "Company"
+        public string? CompanyType { get; set; } // "Airline", "Housing"
+    }
+}
