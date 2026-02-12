fbec3ff1868bff75c14fdaf6340c280566473862
AfonsoOliveira1 <afonso24.oliveira@gmail.com>
2026-02-12 16:00:16 +0000

    esta horribel

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -8,7 +8,6 @@ namespace Booking.Web.Controllers
     public class BookingsController : Controller
     {
         private readonly IHttpClientFactory _clientFactory;
-        private readonly int _dummyUserId = 1;
 
         public BookingsController(IHttpClientFactory clientFactory)
         {
@@ -18,17 +17,39 @@ namespace Booking.Web.Controllers
         // GET: Bookings/Index
         public async Task<IActionResult> Index()
         {
-            // buscar o ID do user
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (string.IsNullOrEmpty(userId)) return RedirectToAction("Login", "Account");
+            int userId = int.Parse(User.FindFirst("id").Value);
 
             var client = _clientFactory.CreateClient("Booking.API");
-            // passar ID aapi
-            var bookings = await client.GetFromJsonAsync<List<BookingViewModel>>($"api/bookings/user/{userId}");
 
-            return View(bookings);
+            // --- CALL FLIGHT API ---
+            var responseFlight = await client.GetAsync($"api/FlightBookingsController/passenger/{userId}");
+
+            IEnumerable<FlightBookingViewModel> flightBookings = new List<FlightBookingViewModel>();
+
+            if (responseFlight.IsSuccessStatusCode)
+            {
+                flightBookings = await responseFlight.Content
+                    .ReadFromJsonAsync<IEnumerable<FlightBookingViewModel>>();
+            }
+
+            // --- CALL HOUSING API ---
+            var responseHouse = await client.GetAsync($"api/HousingBookingController/user/{userId}");
+
+            IEnumerable<HousingBookingViewModel> housingBookings = new List<HousingBookingViewModel>();
+
+            if (responseHouse.IsSuccessStatusCode)
+            {
+                housingBookings = await responseHouse.Content
+                    .ReadFromJsonAsync<IEnumerable<HousingBookingViewModel>>();
+            }
+
+            // --- BUILD UNIFIED HISTORY ---
+            var history = new UserBookingHistory { Flights = flightBookings, Housings = housingBookings};
+
+            
+            return View(history);
         }
+
         // POST: Bookings/Cancel/5
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ Models/UserBookingHistory.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.web.Models
+{
+    public class UserBookingHistory
+    {
+        public IEnumerable<FlightBookingViewModel> Flights { get; set; }
+        public IEnumerable<HousingBookingViewModel> Housings { get; set; }
+
+    }
+}
