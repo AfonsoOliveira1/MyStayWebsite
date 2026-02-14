4a9fb74baf680e647a081b21fd25e2864ac1ee62
Tomas Theotonio <I230051@inete.net>
2026-02-14 21:04:13 +0000

    housings ja estao

--- Controllers/HousingController.cs
+++ Controllers/HousingController.cs
@@ -1,6 +1,7 @@
 ﻿using Booking.web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Security.Claims;
@@ -26,16 +27,63 @@ namespace Booking.Web.Controllers
             return View(housings);
         }
 
-        public async Task<IActionResult> CreateStays()
+
+        [Authorize(Roles = "RENTER,ADMIN")]
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
+            var client = _clientFactory.CreateClient("Booking.API");
+            var response = await client.GetAsync("api/Cities");
+
+            IEnumerable<CityViewModel> cities = new List<CityViewModel>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                cities = await response.Content.ReadFromJsonAsync<IEnumerable<CityViewModel>>()
+                         ?? new List<CityViewModel>();
+            }
+
+            
+            ViewBag.Cities = new SelectList(cities, "Id", "Name");
+
             return View();
         }
-        [Authorize(Roles = "RENTER")]
+        [Authorize(Roles = "RENTER,ADMIN")]
         [HttpPost]
-        public async Task<IActionResult> Create(HousingCreateDto housingDto, IFormFile MainImage, List<IFormFile> SecondaryImages)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HousingCreateDto model)
         {
+            var client = _clientFactory.CreateClient("Booking.API");
 
+            var token = User.FindFirst("JWToken")?.Value;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.PostAsJsonAsync("api/Housings", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["Success"] = "Alojamento enviado para aprovação!";
+                return RedirectToAction("List");
+            }
+
+         
+            var citiesResponse = await client.GetAsync("api/Cities");
+            if (citiesResponse.IsSuccessStatusCode)
+            {
+                var cities = await citiesResponse.Content.ReadFromJsonAsync<IEnumerable<CityViewModel>>();
+
+               
+                ViewBag.Cities = new SelectList(cities, "Id", "Name");
+            }
+            else
+            {
+                ViewBag.Cities = new SelectList(new List<CityViewModel>(), "Id", "Name");
+            }
+
+            return View(model);
         }
+
+
         public async Task<IActionResult> Reserve(int id)
         {
             var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
---
+++ Models/HousingCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Booking.web.Models
+{
+    public class HousingCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal PricePerNight { get; set; } 
+        public long CityId { get; set; }         
+        public string ImageUrl { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
